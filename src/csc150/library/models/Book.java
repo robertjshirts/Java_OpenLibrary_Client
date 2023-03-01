09c9c12e2b272a7e0912f62a125b6b57a4dac044
callanjerel <333221.rob@gmail.com>
2023-03-01 00:59:26 -0700

    JavaDocs

--- src/csc150/library/models/Book.java
+++ src/csc150/library/models/Book.java
@@ -19,6 +19,9 @@ public class Book {
     private String summary;
     private String imageId;
 
+    /**
+     * Default constructor
+     */
     public Book() {
         setTitle(null);
         setPublishDate(null);
@@ -30,8 +33,19 @@ public class Book {
         setImageId(null);
     }
 
+    /**
+     * Constructor that assigns all fields
+     * @param title title
+     * @param publishDate date as string
+     * @param medianPages average number of pages
+     * @param authorNames list of names of authors
+     * @param publisher list of publishers
+     * @param isbn ISBN as a string
+     * @param primaryKey primary edition of the book as a OpenLibrary ID
+     * @param imageId the OpenLibrary ID for the cover image
+     */
     public Book(String title, String publishDate, int medianPages, List<String> authorNames,
-                List<String> subjects, List<String> publisher, String isbn, String primaryKey, String imageId) {
+                List<String> publisher, String isbn, String primaryKey, String imageId) {
         setPrimaryIsbn(isbn);
         setTitle(title);
         setPublishDate(publishDate);
@@ -42,83 +56,159 @@ public class Book {
         setImageId(imageId);
     }
 
+    /**
+     * Return the primary ISBN if it isn't null
+     * @return ISBN or empty string
+     */
     public String getPrimaryIsbn() {
         if (primaryIsbn == null) return "";
         return primaryIsbn + "";
     }
 
+    /**
+     * Sets the primaryISBN
+     * @param primaryIsbn the ISBN to set
+     */
     public void setPrimaryIsbn(String primaryIsbn) {
         this.primaryIsbn = primaryIsbn;
     }
 
+    /**
+     * Returns the title of the primary edition of the book
+     * @return title as a string
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the primary edition of the book
+     * @param title the title to set
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Returns publish date if it isn't null
+     * @return publish date or empty string
+     */
     public String getPublishDate() {
         if (publishDate == null) return "";
         return publishDate + "";
     }
 
+    /**
+     * Sets publish date
+     * @param publishDate the publish date to be set
+     */
     public void setPublishDate(String publishDate) {
         this.publishDate = publishDate;
     }
 
-    public String getMedianPages() {
+    /**
+     * gets the number of pages in the primary edition of the book if it is set
+     * @return the number of pages as a string or an empty string
+     */
+    public String getNumberOfPages() {
         if (medianPages == -1) return "";
         return medianPages + "";
     }
 
-    public void setNumberOfPages(int medianPages) {
-        this.medianPages = medianPages;
+    /**
+     * Set the number of pages in the primary edition of the book
+     * @param numberOfPages the numberOfPages to be set
+     */
+    public void setNumberOfPages(int numberOfPages) {
+        this.medianPages = numberOfPages;
     }
 
+    /**
+     * gets a list of authors, or null if not set
+     * @return the list of authors or null
+     */
     public List<String> getAuthorNames() {
         if (authorNames == null || authorNames.isEmpty()) return null;
         return authorNames;
     }
 
+    /**
+     * Set the authors as a list of strings
+     * @param authorNames the list of authors to be set
+     */
     public void setAuthorNames(List<String> authorNames) {
         this.authorNames = authorNames;
     }
 
+    /**
+     * gets a list of publishers, or null if not set
+     * @return the list of publishers or null
+     */
     public List<String> getPublisher() {
         if (publisher == null || publisher.isEmpty()) return null;
         return publisher;
     }
 
+    /**
+     * Set the publishers as a list of strings
+     * @param publisher the list of publishers to be set
+     */
     public void setPublishers(List<String> publisher) {
         this.publisher = publisher;
     }
 
+    /**
+     * gets the key of the primary edition of the book
+     * @return the OpenLibrary ID of the primary edition of the book
+     */
     public String getPrimaryKey() {
         return primaryKey;
     }
 
+    /**
+     * Sets the key of the primary edition of the book
+     * @param primaryKey the OpenLibrary ID of the primary edition of the book
+     */
     public void setPrimaryKey(String primaryKey) {
         this.primaryKey = primaryKey;
     }
 
+    /**
+     * gets the OpenLibrary ID for a cover image
+     * @return the OpenLibrary ID for the cover image of the primary edition of the book
+     */
     public String getImageId() {
         return imageId;
     }
 
+    /**
+     * Sets the OpenLibrary ID for a cover image
+     * @param imageId the OpenLibrary ID for the cover image of the primary edition of the book
+     */
     public void setImageId(String imageId) {
         this.imageId = imageId;
     }
 
+    /**
+     * gets the summary for the book from the primary edition
+     * @return a summary of the book
+     */
     public String getSummary() {
         return summary;
     }
 
+    /**
+     * Sets the summary for the book from the primary edition
+     * @param summary the summary of the book
+     */
     public void setSummary(String summary) {
         this.summary = summary;
     }
 
+    /**
+     * default toString method from IntelliJ
+     * @return String with fields and values for this object
+     */
     @Override
     public String toString() {
         return "Book{" +
